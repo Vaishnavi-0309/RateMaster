b73bc275e49a2f3a5858348578a03db66ad70a17
Vaishnavi Udipi <113840163+Vaishnavi-0309@users.noreply.github.com>
2026-04-18 11:39:25 +0530

    created project structure

---
+++ ratemaster/ratemaster/src/main/java/com/project/ratemaster/RatemasterApplication.java
@@ -0,0 +1,13 @@
+package com.project.ratemaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatemasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatemasterApplication.class, args);
+	}
+
+}
---
+++ ratemaster/ratemaster/src/test/java/com/project/ratemaster/RatemasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.ratemaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatemasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
